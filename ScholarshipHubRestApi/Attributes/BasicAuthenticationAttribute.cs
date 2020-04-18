e728db179e17ee1aa39c258874f3a81ccf64cd29
Md. Hanif <mdhanif6731@gmail.com>
2020-04-18 14:03:38 +0600

    user api level-2 implemented + basic authentication added

---
+++ ScholarshipHubRestApi/Attributes/BasicAuthenticationAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Security.Principal;
+using ScholarshipHubRestApi.Interfaces;
+using ScholarshipHubRestApi.Repository;
+
+namespace ScholarshipHubRestApi.Attributes
+{
+    public class BasicAuthenticationAttribute:AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            base.OnAuthorization(actionContext);
+            if (actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                string encoded = actionContext.Request.Headers.Authorization.Parameter.ToString();
+                string decoded = Encoding.UTF8.GetString(Convert.FromBase64String(encoded));
+                string[] arr = decoded.Split(new char[]{':'});
+                string username = arr[0];
+                string password = arr[1];
+                IUserRepository uRep = new UserRepository();
+                var user = uRep.GetUser(username);
+                if (username == user.Username && password == user.Password)
+                {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity("admin"), null);
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+            }
+        }
+    }
+}
--- ScholarshipHubRestApi/Controllers/UserController.cs
+++ ScholarshipHubRestApi/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ScholarshipHubRestApi.Interfaces;
+using ScholarshipHubRestApi.Models;
+using ScholarshipHubRestApi.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,33 +10,68 @@ using System.Web.Http;
 
 namespace ScholarshipHubRestApi.Controllers
 {
+    [RoutePrefix("api/users")]
     public class UserController : ApiController
     {
+        IUserRepository uRep = new UserRepository();
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        [Route("")]
+        public IHttpActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var users = uRep.GetAll();
+
+            if (users.Count() > 0)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
         }
 
+        [Route("user", Name ="GetUserByUsername")]
         // GET api/<controller>/5
-        public string Get(int id)
+        public IHttpActionResult Get(User user_)
         {
-            return "value";
+            var user = uRep.GetUser(user_.Username);
+
+            if (uRep.Get(user) == 1)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.NoContent);
+            }
         }
 
+        [Route("")]
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody]User user)
         {
+            uRep.Insert(user);
+            string url = Url.Link("GetUserByUsername", new { user });
+            return Created(url, user);
         }
 
+        [Route("id")]
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public IHttpActionResult Put([FromBody]User user, [FromUri]int id)
         {
+            user.id = id;
+            uRep.Update(user);
+
+            return Ok(user);
         }
 
+        [Route("id")]
         // DELETE api/<controller>/5
-        public void Delete(int id)
+        public IHttpActionResult Delete(int id)
         {
+            uRep.Delete(id);
+            return StatusCode(HttpStatusCode.NoContent);
+
         }
     }
 }
