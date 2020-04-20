5e81da0bca6333355fe14071a5534390926809e2
Md. Hanif <mdhanif6731@gmail.com>
2020-04-20 21:22:08 +0600

    universit offering feature api level-2 implemented

--- ScholarshipHubRestApi/Controllers/UniversityOfferController.cs
+++ ScholarshipHubRestApi/Controllers/UniversityOfferController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ScholarshipHubRestApi.Interfaces;
+using ScholarshipHubRestApi.Models;
+using ScholarshipHubRestApi.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,33 +10,51 @@ using System.Web.Http;
 
 namespace ScholarshipHubRestApi.Controllers
 {
+    [RoutePrefix("api/universities/{uId}/offers")]
     public class UniversityOfferController : ApiController
     {
+        IUniversityOfferRepository offerRep = new UniversityOfferRepository();
+        [Route("")]
         // GET api/<controller>
-        public IEnumerable<string> Get()
+        public IHttpActionResult Get(int uId)
         {
-            return new string[] { "value1", "value2" };
+            var offers = offerRep.GetAll(uId);
+            return Ok(offers);
         }
 
+        [Route("{id}",Name ="GetOfferById")]
         // GET api/<controller>/5
-        public string Get(int id)
+        public IHttpActionResult Get(int uId, int id)
         {
-            return "value";
+            var offer = offerRep.Get(id);
+            return Ok(offer);
         }
 
+        [Route("")]
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public IHttpActionResult Post([FromBody]UniversityOffer universityOffer,[FromUri]int uId)
         {
+            universityOffer.UniversityId = uId;
+            offerRep.Insert(universityOffer);
+            string url = Url.Link("GetOfferById", new {uId=universityOffer.UniversityId,id=universityOffer.id });
+            return Created(url, universityOffer);
         }
 
+        [Route("{id}")]
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
+        public IHttpActionResult Put([FromUri] int uId,[FromUri]int id, [FromBody]UniversityOffer universityOffer)
         {
+            universityOffer.UniversityId = uId;
+            universityOffer.id = id;
+            offerRep.Update(universityOffer);
+            return Ok(universityOffer);
         }
-
+        [Route("{id}")]
         // DELETE api/<controller>/5
-        public void Delete(int id)
+        public IHttpActionResult Delete(int id)
         {
+            offerRep.Delete(id);
+            return StatusCode(HttpStatusCode.NoContent);
         }
     }
 }
